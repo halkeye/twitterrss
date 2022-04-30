8b6104de766a2578fba57c02a4c5164c619e7fb3
Gavin Mogan <git@gavinmogan.com>
2022-04-30 13:18:37 -0700

    it works~

---
+++ go.mod
@@ -0,0 +1,25 @@
+module github.com/halkeye/twitterrss
+
+go 1.17
+
+require (
+	github.com/coreos/pkg v0.0.0-20180928190104-399ea9e2e55f
+	github.com/dghubble/go-twitter v0.0.0-20220428155120-ee736133298b
+	github.com/dghubble/oauth1 v0.7.1
+	github.com/gorilla/feeds v1.1.1
+	github.com/gorilla/handlers v1.5.1
+	github.com/gorilla/mux v1.8.0
+	github.com/pkg/errors v0.9.1
+	golang.org/x/oauth2 v0.0.0-20220411215720-9780585627b5
+)
+
+require (
+	github.com/cenkalti/backoff/v4 v4.1.3 // indirect
+	github.com/dghubble/sling v1.4.0 // indirect
+	github.com/felixge/httpsnoop v1.0.1 // indirect
+	github.com/golang/protobuf v1.4.2 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
+	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd // indirect
+	google.golang.org/appengine v1.6.6 // indirect
+	google.golang.org/protobuf v1.25.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,170 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/coreos/pkg/flagutil"
+	"github.com/dghubble/go-twitter/twitter"
+	"github.com/gorilla/feeds"
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
+	"github.com/pkg/errors"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/clientcredentials"
+)
+
+type arrayFlags []string
+
+func (i *arrayFlags) String() string {
+	return "my string representation"
+}
+
+func (i *arrayFlags) Set(value string) error {
+	*i = append(*i, value)
+	return nil
+}
+
+type flagStruct struct {
+	consumerKey    string
+	consumerSecret string
+	port           int
+	usernames      arrayFlags
+}
+
+func main() {
+	flags := flagStruct{}
+
+	flag.Var(&flags.usernames, "usernames", "Allowed Usernames")
+	flag.StringVar(&flags.consumerKey, "consumer-key", "", "Twitter Consumer Key")
+	flag.StringVar(&flags.consumerSecret, "consumer-secret", "", "Twitter Consumer Secret")
+	flag.IntVar(&flags.port, "port", 8000, "port")
+	flag.Parse()
+	flagutil.SetFlagsFromEnv(flag.CommandLine, "TWITTER")
+
+	if flags.consumerKey == "" || flags.consumerSecret == "" {
+		log.Fatal("Application Access Token required")
+	}
+
+	if os.Getenv("PORT") != "" {
+		port, err := strconv.Atoi(os.Getenv("PORT"))
+		if err != nil {
+			log.Fatal("Invalid port")
+		}
+		flags.port = port
+	}
+
+	r := mux.NewRouter()
+	r.HandleFunc("/healthcheck", HealthCheckHandler)
+
+	for i := 0; i < len(flags.usernames); i++ {
+		url := fmt.Sprintf("/feed/%s", flags.usernames[i])
+		log.Print(url)
+		r.HandleFunc(url, UsernameHandler(flags.usernames[i], flags.consumerKey, flags.consumerSecret))
+	}
+
+	loggedRouter := handlers.LoggingHandler(os.Stdout, r)
+	log.Printf("Listening on :%d\n", flags.port)
+	http.ListenAndServe(fmt.Sprintf(":%d", flags.port), Recovery(handlers.ProxyHeaders(loggedRouter)))
+}
+
+func Recovery(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		defer func() {
+			err := recover()
+			if err != nil {
+
+				jsonBody, _ := json.Marshal(map[string]string{
+					"error": "There was an internal server error",
+				})
+
+				w.Header().Set("Content-Type", "application/json")
+				w.WriteHeader(http.StatusInternalServerError)
+				w.Write(jsonBody)
+
+				log.Fatal(err)
+			}
+
+		}()
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+func HealthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	response := map[string]string{}
+
+	jsonBody, err := json.Marshal(response)
+	if err != nil {
+		panic(errors.Wrap(err, "Unable to create response"))
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(jsonBody)
+}
+
+func UsernameHandler(username string, consumerKey string, consumerSecret string) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// oauth2 configures a client that uses app credentials to keep a fresh token
+		config := &clientcredentials.Config{
+			ClientID:     consumerKey,
+			ClientSecret: consumerSecret,
+			TokenURL:     "https://api.twitter.com/oauth2/token",
+		}
+		// http.Client will automatically authorize Requests
+		httpClient := config.Client(oauth2.NoContext)
+
+		// Twitter client
+		client := twitter.NewClient(httpClient)
+
+		// Status Show
+		tweets, _, err := client.Timelines.UserTimeline(&twitter.UserTimelineParams{
+			ScreenName:     username,
+			ExcludeReplies: twitter.Bool(true),
+		})
+
+		if err != nil {
+			panic(errors.Wrap(err, "Unable to get tweets"))
+		}
+
+		feed := &feeds.Feed{
+			Title:       fmt.Sprintf("%s tweets", "dgSHiFTCodes"),
+			Link:        &feeds.Link{Href: "your feed link"},
+			Description: fmt.Sprintf("%s tweets", "dgSHiFTCodes"),
+			Author:      &feeds.Author{Name: "https://github.com/halkeye/twitterrss"},
+			Created:     time.Now(),
+		}
+
+		var feedItems []*feeds.Item
+		for i := 0; i < len(tweets); i++ {
+			tweet := tweets[i]
+			createdAt, _ := tweet.CreatedAtTime()
+			feedItems = append(feedItems,
+				&feeds.Item{
+					Id:          tweet.IDStr,
+					Title:       tweet.IDStr,
+					Link:        &feeds.Link{Href: tweet.Source},
+					Description: tweet.Text,
+					Created:     createdAt,
+				})
+		}
+
+		feed.Items = feedItems
+
+		rss, err := feed.ToRss()
+		if err != nil {
+			panic(errors.Wrap(err, "unable to create rss feed"))
+		}
+
+		w.Header().Set("Content-Type", "application/rss+xml")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(rss))
+	}
+}
